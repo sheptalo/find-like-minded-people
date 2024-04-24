31d20b6f25c35dc2e0ece787c838dd355233959c
sheptalo <solovovartem99@gmail.com>
2024-04-24 20:50:49 +0300

    начал делать логин

--- Userprofile/models.py
+++ Userprofile/models.py
@@ -6,7 +6,6 @@ from django.db import models
 class UserProfileModel(models.Model):
 
     user_main = models.ForeignKey(User, on_delete=models.CASCADE)
-    registered_date = models.DateTimeField()
 
     def __str__(self):
         return self.user_main.username
--- Userprofile/views.py
+++ Userprofile/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth import authenticate, logout, login
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
@@ -16,13 +17,24 @@ def profile(request, username=None):
     return HttpResponse("None")
 
 
-def login(request):
-    pass
+def authorize(request, _type):
+    user = request.user
+
+    if (_type == 'login' or _type == 'register') and request.method == 'GET':
+        return render(request, 'User/authorize.html', {'tab': _type, 'user': str(user), 'error': '0'})
+    # elif request.method == 'POST' and request.user.is_anonymous:
+    #     user = authenticate(username=request.POST['username'], password=request.POST['password'])
+    #     if user:
+    #         login(request, user)
+    #         return HttpResponseRedirect('/')
+    #     return render(request, 'User/authorize.html', {
+    #         'tab': _type,
+    #         'user': str(user),
+    #         'error': 'Неправильный пароль или имя',
+    #     })
+    else:
+        return HttpResponseRedirect('/')
 
 
-def logout(request):
-    pass
-
-
-def register(request):
-    pass
+def logout_func(request):
+    logout(request)
