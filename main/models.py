1a7f1d33aadb371e323937d06e77d5796249d3a8
Samir_1337 <ssodirjonzoda@gmail.com>
2024-04-24 21:09:53 +0300

    Побровал сделать модель постов добавил заголовок ну кароче посмотришь)

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+class MyModel(models.Model):
+    field_name = models.CharField(max_length=100)
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    body = models.TextField()
+    def __str__(self): return self.title
+    def get_absolute_url(self): return reverse('post_detail', args=[self.id])
+
